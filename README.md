0a269316f4e61d23d155846d37f54d5e77786484
bugsounet <bugsounet@bugsnana.fr>
2020-09-14 14:18:31 +0200

    Installer @bugsounet style ;)

---
+++ README.md
@@ -0,0 +1,5 @@
+# MMM-Pronote
+
+Coding in Progress ...
+
+![](https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif)
---
+++ installer/preinstall.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# +----------------+
+# | npm preinstall |
+# +----------------+
+
+# with or without prompt ?
+prompt=true
+if [ -e no-prompt ]; then
+  prompt=false
+fi
+
+# get the installer directory
+Installer_get_current_dir () {
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+  echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+Installer_dir="$(Installer_get_current_dir)"
+
+# move to installler directory
+cd "$Installer_dir"
+
+source utils.sh
+
+# module name
+Installer_module="MMM-Pronote"
+Version="$(node -p -e "require('./../package.json').version")"
+Installer_info "Welcome to $Installer_module v$Version"
+
+# delete package-lock.json (force)
+rm -f ../package-lock.json
+
+# Check not run as root
+if [ "$EUID" -eq 0 ]; then
+  Installer_error "npm install must not be used as root"
+  exit 1
+fi
+
+echo
+
+# switch branch
+Installer_info "Installing Sources..."
+git checkout -f dev 2>/dev/null || Installer_error "Installing Error !"
+git pull 2>/dev/null
+
+echo
+Installer_info "Installing all npm libraries..."
